8a71376f55eec516dbfa5628bc2854783bb8cc24
alirezafay <149387054+alirezafay@users.noreply.github.com>
2024-07-02 15:41:46 +0330

    Update train.py

--- train.py
+++ train.py
@@ -133,16 +133,16 @@ def main():
                 loss_generator = 0
                 loss_discriminator_i = 0
                 loss_discriminator_v = 0
-		for i , (pet, image) in enumerate(loader):
+                for i , (pet, image) in enumerate(loader):
 			model,loss_g=train_Generator(model,l_min,Loss_G,Loss_adv_G,Loss_Dv,Loss_Di,vis,ir,max_epoch,args.lr)
                         loss_generator += loss_g.item()
                         model,loss_G,loss_i,loss_v=train_Discriminators(model,l_max,Loss_G,Loss_Dv,Loss_Di,vis,ir,max_epoch,args.lr)
                         loss_discriminator_i = loss_i.item()
                         loss_discriminator_v = loss_v.item()
                 torch.save(model,'/kaggle/working'+str(epoch)+'.pth')
-		print(f'Loss discriminator infrared: {loss_discriminator_i}')
-		print(f'Loss discriminator visible: {loss_discriminator_v}')
-		print(f'Loss Generator: {loss_generator}')
+                print(f'Loss discriminator infrared: {loss_discriminator_i}')
+                print(f'Loss discriminator visible: {loss_discriminator_v}')
+                print(f'Loss Generator: {loss_generator}')
 
 if __name__=='__main__':
 	main()
